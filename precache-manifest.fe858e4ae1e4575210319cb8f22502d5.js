5133fc7e200d3040c16d87c26517096bfc54518c
Kool Havis <havisk@gmail.com>
2019-07-02 15:53:45 -0400

    Updates

---
+++ precache-manifest.fe858e4ae1e4575210319cb8f22502d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4777f9a207dbd0c5871c5a1ef4b1dfc7",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "cde6d8722c0386a6aca3",
+    "url": "/monster-rolodex/static/css/main.63d60af0.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "cde6d8722c0386a6aca3",
+    "url": "/monster-rolodex/static/js/main.f529a779.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
